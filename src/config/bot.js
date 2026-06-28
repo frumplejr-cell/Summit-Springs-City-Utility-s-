bebf3c65a12a77870b91fc0523836f1b52984762
frumplejr-cell <frumplejr@gmail.com>
2026-06-28 07:26:18 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,7 +11,8 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "online",
+    status: "online"
+      ",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -24,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "SSRP BOT
+        name: "SSRP BOT"
           ",
         // Activity type number (0 = Playing).
         type: 0,
